a5cd1936ee066a939ca6a1194542a70d5a97230b
nktk <eztestmail443@gmail.com>
2019-01-09 11:09:08 +0900

    add test

---
+++ test/float_test.ml
@@ -0,0 +1,17 @@
+print_float(-12.3);
+print_newline ();
+print_float(abs_float(-12.3));
+print_newline ();
+(*print_float(sqrt(2.0));*)
+print_int(int_of_float(floor(2.3)));
+print_newline ();
+print_int(truncate(10.3));
+print_newline ();
+print_float(float_of_int(4) +. 0.1);
+print_newline ();
+(*print_float(cos(3.14));
+print_newline ();
+print_float(sin(3.14));
+print_newline ();
+print_float(atan(3.14 /. 2.0));*)
+print_newline ()
---
+++ test/read_test.ml
@@ -0,0 +1,4 @@
+print_int(read_int());
+print_newline();
+print_float(read_float());
+print_newline()
